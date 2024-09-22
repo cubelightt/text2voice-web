4dadb186381d8c95851a48dd41a6aff7c3fc6adc
wancube <70363928+cubelightt@users.noreply.github.com>
2024-09-22 14:05:22 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const apiConfig = {
     "voice-api": {
-        url: "https://ttsapi.zwei.de.eu.org/tts",
+        url: "https://voice-api.455100.xyz/tts",
         speakers: {
             "zh-CN-XiaoxiaoNeural": "晓晓",
             "zh-CN-YunxiNeural": "云希",
@@ -137,7 +137,7 @@ function generateVoice(isPreview) {
         url: url,
         method: 'GET',
         headers: {
-            'x-api-key': '@ak47'  // 添加 API 密钥
+            'x-api-key': '@hk416'  // 添加 API 密钥
         },
         xhrFields: {
             responseType: 'blob' // 确保返回的是一个Blob对象
